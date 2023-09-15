9d085f5c2b0ccbb27bf12844299445026c1a78ea
himanshu8080 <76471257+himanshu8080@users.noreply.github.com>
2023-09-16 00:29:03 +0530

    updating and resolving error in navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,144 +1,194 @@
-import React from 'react'
+// import React from 'react'
 
 
+// const Navbar = () => {
+//   return (  
+//     <div className='ipl_navbar'>
+//       <div className="navbar navbar-expand-lg navbar-light bg-light">
+//     {/* Container wrapper */}
+//     <div className="container-fluid">
+//       {/* Toggle button */}
+//       <button
+//         className="navbar-toggler"
+//         type="button"
+//         data-mdb-toggle="collapse"
+//         data-mdb-target="#navbarSupportedContent"
+//         aria-controls="navbarSupportedContent"
+//         aria-expanded="false"
+//         aria-label="Toggle navigation"
+//       >
+//         <i className="fas fa-bars" />
+//       </button>
+//       {/* Collapsible wrapper */}
+//       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//         {/* Navbar brand */}
+//         <link className="navbar-brand mt-2 mt-lg-0" to='/' >
+//           <img
+//             src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+//             height={15}
+//             alt="MDB Logo"
+//             loading="lazy"
+//           />
+//         </link>
+//         {/* Left links */}
+//         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//           <li className="nav-item">
+//             <link className="nav-link" to='/'>
+//               Dashboard
+//             </link>
+//           </li>
+//           <li className="nav-item">
+//             <link className="nav-link" to='/'>
+//               Team
+//             </link>
+//           </li>
+//           <li className="nav-item">
+//             <link className="nav-link" to='/'>
+//               Projects
+//             </link>
+//           </li>
+//         </ul>
+//         {/* Left links */}
+//       </div>
+//       {/* Collapsible wrapper */}
+//       {/* Right elements */}
+//       <div className="d-flex align-items-center">
+//         {/* Icon */}
+//         <link className="text-reset me-3" to='/'>
+//           <i className="fas fa-shopping-cart" />
+//         </link>
+//         {/* Notifications */}
+//         <div className="dropdown">
+//           <link
+//             className="text-reset me-3 dropdown-toggle hidden-arrow"
+//             to='/'
+//             id="navbarDropdownMenuLink"
+//             type="button"
+//             data-mdb-toggle="dropdown"
+//             aria-expanded="false"
+//           >
+//             <i className="fas fa-bell" />
+//             <span className="badge rounded-pill badge-notification bg-danger">
+//               1
+//             </span>
+//           </link>
+//           <ul
+//             className="dropdown-menu dropdown-menu-end"
+//             aria-labelledby="navbarDropdownMenuLink"
+//           >
+//             <li>
+//               <link className="dropdown-item" to='/'>
+//                 Some news
+//               </link>
+//             </li>
+//             <li>
+//               <link className="dropdown-item" to='/'>
+//                 Another news
+//               </link>
+//             </li>
+//             <li>
+//               <link className="dropdown-item" to='/'>
+//                 Something else here
+//               </link>
+//             </li>
+//           </ul>
+//         </div>
+//         {/* Avatar */}
+//         <div className="dropdown">
+//           <link
+//             className="dropdown-toggle d-flex align-items-center hidden-arrow"
+//             to='/'
+//             id="navbarDropdownMenuAvatar"
+//             role="button"
+//             data-mdb-toggle="dropdown"
+//             aria-expanded="false"
+//           >
+//             <img
+//               src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+//               className="rounded-circle"
+//               height={25}
+//               alt="Black and White Portrait of a Man"
+//               loading="lazy"
+//             />
+//           </link>
+//           <ul
+//             className="dropdown-menu dropdown-menu-end"
+//             aria-labelledby="navbarDropdownMenuAvatar"
+//           >
+//             <li>
+//               <link className="dropdown-item" to='/'>
+//                 My profile
+//               </link>
+//             </li>
+//             <li>
+//               <link className="dropdown-item" to='/'>
+//                 Settings
+//               </link>
+//             </li>
+//             <li>
+//               <link className="dropdown-item" to='/'>
+//                 Logout
+//               </link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//       {/* Right elements */}
+//     </div>
+//     {/* Container wrapper */}
+//   </div>
+//     </div>
+//   )
+// }
+
+// export default Navbar;
+
+import React, { useState } from 'react';
+import {RiMenu3Line,RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.png';
+import './Navbar.css';
 const Navbar = () => {
-  return (  
-    <div className='ipl_navbar'>
-      <div className="navbar navbar-expand-lg navbar-light bg-light">
-    {/* Container wrapper */}
-    <div className="container-fluid">
-      {/* Toggle button */}
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i className="fas fa-bars" />
-      </button>
-      {/* Collapsible wrapper */}
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        {/* Navbar brand */}
-        <a className="navbar-brand mt-2 mt-lg-0" href="#">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-            height={15}
-            alt="MDB Logo"
-            loading="lazy"
-          />
-        </a>
-        {/* Left links */}
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Dashboard
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Team
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Projects
-            </a>
-          </li>
-        </ul>
-        {/* Left links */}
-      </div>
-      {/* Collapsible wrapper */}
-      {/* Right elements */}
-      <div className="d-flex align-items-center">
-        {/* Icon */}
-        <a className="text-reset me-3" href="#">
-          <i className="fas fa-shopping-cart" />
-        </a>
-        {/* Notifications */}
-        <div className="dropdown">
-          <a
-            className="text-reset me-3 dropdown-toggle hidden-arrow"
-            href="#"
-            id="navbarDropdownMenuLink"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <i className="fas fa-bell" />
-            <span className="badge rounded-pill badge-notification bg-danger">
-              1
-            </span>
-          </a>
-          <ul
-            className="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdownMenuLink"
-          >
-            <li>
-              <a className="dropdown-item" href="#">
-                Some news
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another news
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
+  const[toggleMenu, setToggleMenu]=useState(false);
+  return (
+    <div className='IPL__navbar'>
+      <div className='IPL__navbar-links'>
+        <div className='IPL__navbar-links_logo'>
+          <img src={logo} alt="IPL__logo"/>
         </div>
-        {/* Avatar */}
-        <div className="dropdown">
-          <a
-            className="dropdown-toggle d-flex align-items-center hidden-arrow"
-            href="#"
-            id="navbarDropdownMenuAvatar"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-              className="rounded-circle"
-              height={25}
-              alt="Black and White Portrait of a Man"
-              loading="lazy"
-            />
-          </a>
-          <ul
-            className="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdownMenuAvatar"
-          >
-            <li>
-              <a className="dropdown-item" href="#">
-                My profile
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Logout
-              </a>
-            </li>
-          </ul>
+        <div className='IPL__navbar-links_container'>
+        <p><a href='#home'>Home</a></p>
+        <p><a href='https://github.com/himanshu8080/IPL_server'>Read More</a></p>
+  <p><a href='#What-Is-IPL'>More About Us</a></p>
+  
+  {/* <p><a href='#features'>Case Studies</a></p> */}
+  {/* <p><a href='#blog'>Sources</a></p> */}
         </div>
       </div>
-      {/* Right elements */}
+      {/* <div className='IPL__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div> */}
+      <div className='IPL__navbar-menu'>
+        {toggleMenu
+          ?<RiCloseLine color='#fff' size={27} onClick={()=> setToggleMenu(false)} />
+          :<RiMenu3Line color='#fff' size={27} onClick={()=> setToggleMenu(true)} />}
+        {toggleMenu && (
+          <div className="IPL__navbar-menu_container scale-up-center">
+            <div className='IPL__navbar-menu_container-links'>
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#What-Is-IPL'>More About Us</a></p>
+            {/* <p><a href='#hpossibility'>Library</a></p> */}
+            <p><a href='#features'>Case Studies</a></p>
+            <p><a href='#blog'>Sources</a></p>
+            </div>
+            <div className='IPL__navbar-menu_container-links-sign'>
+              <p>Sign in</p>
+                <button type='button'>Sign up</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-    {/* Container wrapper */}
-  </div>
-    </div>
-  )
-}
-
-export default Navbar;
+  ) 
+};
+export default Navbar
